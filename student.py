c9aa3c28203a872873d6e87504097c7402ad59b5
swehrwein <scott.wehrwein@wwu.edu>
2020-05-18 21:08:37 -0700

    initial commit

---
+++ student.py
@@ -0,0 +1,81 @@
+import numpy as np
+
+
+def project_impl(K, Rt, points):
+    """
+    Project 3D points into a calibrated camera.
+    Input:
+        K -- 3 x 3 camera intrinsics calibration matrix
+        Rt -- 3 x 4 camera extrinsics calibration matrix
+        points -- height x width x 3 array of 3D points
+    Output:
+        projections -- height x width x 2 array of 2D projections
+    """
+    raise NotImplementedError()
+
+
+def preprocess_ncc_impl(image, ncc_size):
+    """
+    Prepare normalized patch vectors according to normalized cross
+    correlation.
+
+    This is a preprocessing step for the NCC pipeline.  It is expected that
+    'preprocess_ncc' is called on every input image to preprocess the NCC
+    vectors and then 'compute_ncc' is called to compute the dot product
+    between these vectors in two images.
+
+    NCC preprocessing has two steps.
+    (1) Compute and subtract the mean.
+    (2) Normalize the vector.
+
+    The mean is per channel.  i.e. For an RGB image, over the ncc_size**2
+    patch, compute the R, G, and B means separately.  The normalization
+    is over all channels.  i.e. For an RGB image, after subtracting out the
+    RGB mean, compute the norm over the entire (ncc_size**2 * channels)
+    vector and divide.
+
+    If the norm of the vector is < 1e-6, then set the entire vector for that
+    patch to zero.
+
+    Patches that extend past the boundary of the input image at all should be
+    considered zero.  Their entire vector should be set to 0.
+
+    Patches are flattened into vectors with the default numpy row
+    major order.  For example, the following 3D numpy array with shape
+    2 (channels) x 2 (height) x 2 (width) patch...
+
+    channel1         channel2
+    +------+------+  +------+------+ height
+    | x111 | x121 |  | x112 | x122 |  |
+    +------+------+  +------+------+  |
+    | x211 | x221 |  | x212 | x222 |  |
+    +------+------+  +------+------+  v
+    width ------->
+
+    gets unrolled using np.reshape into a vector in the following order:
+
+    v = [ x111, x121, x211, x221, x112, x122, x212, x222 ]
+
+    Input:
+        image -- height x width x channels image of type float32
+        ncc_size -- integer side length of (square) NCC patch region; must be odd
+    Output:
+        normalized -- height x width x (channels * ncc_size**2) array
+    """
+    raise NotImplementedError()
+
+
+def compute_ncc_impl(image1, image2):
+    """
+    Compute normalized cross correlation between two images that already have
+    normalized vectors computed for each pixel with preprocess_ncc.
+
+    Input:
+        image1 -- height x width x (channels * ncc_size**2) array
+        image2 -- height x width x (channels * ncc_size**2) array
+    Output:
+        ncc -- height x width normalized cross correlation between image1 and
+               image2.
+    """
+    raise NotImplementedError()
+
